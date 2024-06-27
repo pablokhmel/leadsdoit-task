8750d93904088786022502f1c62865b481b0609b
pablokhmel <pablokhmelevskiy@gmail.com>
2024-06-27 20:13:34 +0200

    Initial Commit

---
+++ LeadDoIt Test Task/LeadDoIt_Test_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeadDoIt_Test_TaskApp.swift
+//  LeadDoIt Test Task
+//
+//  Created by MacBook on 27.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct LeadDoIt_Test_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
